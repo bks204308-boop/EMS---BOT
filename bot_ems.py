cfdfb92b4167d39ad6328b6d8621b2112d2d662f
bks204308-boop <bks204308@gmail.com>
2026-08-12 12:36:44 +0200

    Update bot_ems.py

--- bot_ems.py
+++ bot_ems.py
@@ -24,8 +24,6 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
- :
-            resultats else None
 
 # ---------- BASE DE DONNÉES (PostgreSQL) ----------
 import asyncpg
